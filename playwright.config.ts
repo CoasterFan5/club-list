17411d50a3172bb77dabaaf7b9aa88197683aa2c
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-12-04 09:36:15 -0500

    Change configured playwright port

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && node build',
-		port: 4173
+		port: 3000
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
