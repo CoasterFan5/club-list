8b1e98f04b297d551324f17e87b381d0346d4fcf
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-12-04 10:04:11 -0500

    add club settings form

---
+++ src/routes/(app)/org/[id]/club/[clubId]/settings/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+import Input from "$lib/components/Input.svelte";
+	import type { PageData } from "./$types"
+
+	export let data: PageData;
+
+	let name = data.club.name || "";
+	let imgURL = data.club.imageURL || "";
+
+</script>
+
+
+
+<div class="wrap">
+	<form method="post" action="?/updateClub">
+		<div class="formItem">
+			<Input name="clubName" label="Club Name" value={name}/>
+		</div>
+		<div class="formItem">
+			<Input name="imgURL" label="Image Url" value={imgURL}/>
+		</div>
+		<div class="formItem">
+			<Button type="submit" value="Save"/>
+		</div>
+		
+		
+		
+	</form>
+</div>
+
+<style>
+	.wrap {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: start;
+		justify-content: center;
+	}
+	.formItem {
+		width: 100%;
+		margin: 7px 0px;
+	}
+	form {
+		padding: 50px;
+		background: var(--bgPure);
+		box-sizing: border-box;
+		max-width: 500px;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 50px;
+		justify-content: center;
+	}
+</style>
