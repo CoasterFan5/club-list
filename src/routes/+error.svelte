37ca11017fc8a857163c6f0735aac059aa056b35
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-15 16:37:19 -0500

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<main>
+    {#if $page.error}
+        <h1>{$page.status} <span class="bar">|</span> <span class="error">{$page.error.message}</span></h1>
+    {/if}
+
+    <div class="small">
+        <Button href="/" value="Go Home" />
+    </div>
+</main>
+
+<style lang="scss">
+    h1 {
+        font-weight: 400;
+
+        .bar {
+            font-weight: 100;
+        }
+
+        .error {
+            color: var(--accent);
+            font-weight: 300;
+        }
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .small {
+        width: 100%;
+        max-width: 400px;
+        padding: 2rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
