c5610f65ac422ce7fa8431cd664908edc9da0913
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-16 15:42:05 -0500

    test: dotenv with node build

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'pnpm run build && node build',
+		command: 'pnpm run build && pnpm exec dotenv node build',
 		port: 3000,
 		reuseExistingServer: true
 	},
