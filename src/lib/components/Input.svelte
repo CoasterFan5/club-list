db7bf45a840aed7150f4e6bac46eb9caf2c1a2f2
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-16 11:13:55 -0500

    feat: much cooler inputs

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,62 +1,80 @@
 <script lang="ts">
 	export let name = 'Input';
 	export let label = 'Input';
-	export let bgColor = 'var(--bg)';
 	export let type: 'password' | 'email' | undefined = undefined;
 	export let required = false;
 	export let autocomplete: HTMLInputElement['autocomplete'] | null = null;
 
 	export let value: string = '';
-
-	let active = false;
 </script>
 
-<label style="--background-color: {bgColor}" class:active>
-	<input {name} {autocomplete} placeholder=" " {required} bind:value {...{ type }} />
-	<span class="labelBase">
-		{label}
-	</span>
-</label>
+<div>
+	<input {name} {autocomplete} placeholder={label} {required} bind:value {...{ type }} />
+	<label for={name}>{label}</label>
+</div>
 
 <style lang="scss">
-	label {
+	$gray: #9b9b9b;
+
+	div {
 		position: relative;
-		--bezel: 0.5rem;
+		padding: 15px 0 0;
+		width: 100%;
+	}
 
-		span {
-			position: absolute;
-			left: 0;
-			top: 0;
-			padding: calc(var(--bezel) * 0.75) calc(var(--bezel) * 0.5);
-			margin: calc(var(--bezel) * 2.2 + 3px) calc(var(--bezel) * 0.5);
-			background: pink;
-			white-space: nowrap;
-			transform: translate(0, 0);
-			transform-origin: 0 0;
-			background: var(--background-color);
-			transition: transform 120ms ease-in;
-			font-weight: 400;
-			line-height: 1.2;
-			color: #333;
+	input {
+		font-family: inherit;
+		width: 100%;
+		border: 0;
+		border-bottom: 2px solid #9b9b9b;
+		outline: 0;
+		font-size: 1.3rem;
+		color: black;
+		padding: 7px 0;
+		background: transparent;
+		transition: border-color 0.2s;
+
+		&::placeholder {
+			color: transparent;
 		}
-		input {
-			box-sizing: border-box;
-			display: block;
-			width: 100%;
-			border: 1px solid gray;
-			padding: calc(var(--bezel) * 1.5) var(--bezel);
-			color: currentColor;
-			background: transparent;
-			border-radius: 4px;
 
-			&:focus,
-			&:not(:placeholder-shown) {
-				& + span {
-					transform: translate(0.25rem, -65%) scale(0.8);
-					color: var(--accent);
-				}
+		&:placeholder-shown ~ label {
+			font-size: 1.3rem;
+			cursor: text;
+			top: 20px;
+		}
+
+		&:required,
+		&:invalid {
+			box-shadow: none;
+		}
+
+		&:focus {
+			~ label {
+				position: absolute;
+				top: 0;
+				display: block;
+				transition: 0.2s;
+				font-size: 1rem;
+				color: var(--accent);
+				font-weight: 700;
 			}
+			padding-bottom: 6px;
+			font-weight: 700;
+			border-width: 3px;
+			border-image: linear-gradient(to right, var(--accent), var(--accent));
+			border-image-slice: 1;
 		}
+	}
+
+	label {
+		position: absolute;
+		top: 0;
+		display: block;
+		transition: 0.2s;
+		font-size: 1rem;
+		color: $gray;
+		pointer-events: none;
 	}
 
 	.active {
