009e5c0b67d9cbd1737c5ef5675ff735e5be2341
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-18 18:58:54 -0500

    fix: use 0th get started element

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -16,7 +16,7 @@ test('index page icon redirects to index page', async ({ page }) => {
 
 test('get started button redirects to get started page', async ({ page }) => {
 	await page.goto('/');
-	await page.getByRole('link', { name: 'Get Started' }).click();
+	await page.getByRole('link', { name: 'Get Started' }).nth(0).click();
 	await page.waitForURL('/get-started');
 });
 
