49929cbf40bd901138604c80367a7e59bb38ca66
LeoDog896 <LeoDog896@hotmail.com>
2023-12-19 13:14:22 +0000

    feat: add rate limiter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,44 @@
 import { S3 } from '$lib/s3';
 import { bucket } from '$env/static/private';
 import { ListObjectsV2Command } from '@aws-sdk/client-s3';
+import { RetryAfterRateLimiter } from 'sveltekit-rate-limiter/server';
+import { dev } from '$app/environment';
+import crypto from 'crypto';
+
+const limiter = new RetryAfterRateLimiter({
+	rates: {
+		IP: [5, 's'],
+		IPUA: [5, 's'],
+		cookie: {
+			name: 'limiterid',
+			secret: crypto.getRandomBytes(),
+			rate: [2, 'm'],
+			preflight: true
+		}
+	}
+});
 
 // Check if we have access to an s3 bucket before the app starts
 const bucketCheck = await S3.send(new ListObjectsV2Command({ Bucket: bucket, MaxKeys: 1 }));
 if (bucketCheck.$metadata.httpStatusCode != 200) {
 	throw new Error('s3 Bucket not found');
+}
+
+export async function handle({ event, resolve }) {
+	if (!process.env.CI && !dev) {
+		await limiter.cookieLimiter?.preflight(event);
+
+		const status = await limiter.check(event);
+
+		if (status.limited) {
+			return new Response('Too many requests', {
+				status: 429,
+				headers: {
+					'Retry-After': status.retryAfter.toString()
+				}
+			});
+		}
+	}
+
+	return await resolve(event);
 }
