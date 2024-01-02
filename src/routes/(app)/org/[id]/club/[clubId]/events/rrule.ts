ab794b201f469e97c7310843f2acb84fe0c8b8dd
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-01-02 15:48:52 -0500

    AAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/routes/(app)/org/[id]/club/[clubId]/events/rrule.ts
+++ src/routes/(app)/org/[id]/club/[clubId]/events/rrule.ts
@@ -1,7 +1,7 @@
-import { browser } from '$app/environment';
+import { browser, dev } from '$app/environment';
 import * as rrule from 'rrule';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const { RRule } = browser ? rrule : (rrule as any).default as typeof rrule;
+const { RRule } = browser || !dev ? rrule : (rrule as any).default as typeof rrule;
 
 export { RRule };
