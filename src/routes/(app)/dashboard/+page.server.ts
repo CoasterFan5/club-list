f2c969982abbeb3db40e10dd3c49c691a8ba3204
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-14 21:46:24 -0500

    feat: recent announcements

---
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -0,0 +1,42 @@
+import { prisma } from '$lib/prismaConnection';
+
+export const load = async ({ parent }) => {
+	const { user } = await parent();
+
+	const recentAnnouncements = await prisma.announcement.findMany({
+		where: {
+			club: {
+				OR: [
+					{
+						clubUsers: {
+							some: {
+								userId: user.id
+							}
+						}
+					},
+					{
+						ownerId: {
+							equals: user.id
+						}
+					}
+				]
+			}
+		},
+		take: 3,
+		orderBy: {
+			createdAt: 'desc'
+		},
+		include: {
+			author: {
+				select: {
+					firstName: true,
+					lastName: true
+				}
+			}
+		}
+	});
+
+	return {
+		recentAnnouncements
+	};
+};
--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+    import Announcement from '$lib/components/Announcement.svelte';
+    
+    export let data;
+</script>
+
 <h1>Dashboard</h1>
 
-<h2>Recent Announcements</h2>
+{#if data.recentAnnouncements.length > 0}
+    <h2>Recent Announcements</h2>
+
+    <div class="announcements">
+        {#each data.recentAnnouncements as announcement}
+            <Announcement {announcement} />
+        {/each}
+    </div>
+{/if}
