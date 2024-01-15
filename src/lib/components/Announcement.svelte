557da3155e251c7e073c601800e8296564ec317b
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-15 10:49:28 -0500

    fix: make announcements non-saveable

--- src/lib/components/Announcement.svelte
+++ src/lib/components/Announcement.svelte
@@ -28,7 +28,7 @@
 		</p>
 	{/if}
 	<div class="editor">
-		<MdEditor content={announcement.description || 'No Announcement'} editable={false} />
+		<MdEditor content={announcement.description || 'No Announcement'} editable={false} saveable={false} />
 	</div>
 </div>
 
