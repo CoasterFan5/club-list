3622edc2982c8b879de027c58557486cc6ce341f
Tristan F. <LeoDog896@hotmail.com>
2024-01-17 14:55:22 +0000

    feat: refresh join code

---
+++ src/routes/(app)/org/[id=integer]/settings/danger/+page.server.ts
@@ -0,0 +1,27 @@
+import { prisma } from '$lib/server/prismaConnection.js';
+import { verifySession } from '$lib/server/verifySession.js';
+
+export const actions = {
+	refreshJoinCode: async ({ cookies, params }) => {
+        await verifySession(cookies.get('session'));
+
+        // TODO: better join code generation
+        const orgAmount = await prisma.organization.count();
+        const random = Math.round(Math.random() * 324000 + 36000).toString(36);
+        const joinString = (orgAmount + 1).toString(36) + random;
+
+        await prisma.organization.update({
+            where: {
+                id: parseInt(params.id)
+            },
+            data: {
+                joinCode: joinString
+            }
+        })
+
+        return {
+            success: true,
+            message: 'Organization Updated!'
+        };
+    }
+};
--- src/routes/(app)/org/[id=integer]/settings/danger/+page.svelte
+++ src/routes/(app)/org/[id=integer]/settings/danger/+page.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
+	import { pushState } from "$app/navigation";
+	import { page } from '$app/stores';
+
+	import Modal from '$lib/modules/Modal.svelte';
 	import Button from '$lib/components/Button.svelte';
 </script>
+
+{#if $page.state.showingModal === 'refreshJoinCode'}
+	<Modal on:close={() => history.back()}>
+		<form method="POST" action="?/refreshJoinCode">
+			<h2>Are you sure?</h2>
+			<p>
+				Refreshing the join code
+				will break every existing invite.
+			</p>
+			<Button value="Refresh" />
+		</form>
+	</Modal>
+{/if}
 
 <main>
 	<h2>Danger Zone</h2>
 
 	<form>
-		<Button value="Refresh Join Code" />
+		<Button on:click={() => {
+			pushState('', {
+				showingModal: 'refreshJoinCode'
+			})
+		}} value="Refresh Join Code" />
 	</form>
 	<form>
 		<Button value="Transfer Ownership" />
