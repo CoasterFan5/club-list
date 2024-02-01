d9c7690df6e8c7dcd464e0c7bb2094f160641a5b
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-01 08:11:03 -0500

    filters

--- src/routes/(app)/calendar/+page.server.ts
+++ src/routes/(app)/calendar/+page.server.ts
@@ -1,21 +1,57 @@
 import { prisma } from "$lib/server/prismaConnection"
 import { verifySession } from "$lib/server/verifySession"
 
-export const load = async ({cookies}) => {
+export const load = async ({cookies, url}) => {
 
 	const user = await verifySession(cookies.get("session"))
 
-	const events = await prisma.event.findMany({
-		where: {
-			club: {
-				clubUsers: {
-					some: {
-						userId: user.id
+	
+	enum filter {
+		club = "club",
+		org = "org"
+	};
+	let activeFilter: filter = filter.club
+
+	if(url.searchParams.has("filter")) {
+		if(url.searchParams.get("filter") == "club") {
+			activeFilter = filter.club
+		} else if (url.searchParams.get('filter') == "org") {
+			activeFilter = filter.org
+		} else {
+			activeFilter = filter.club
+		}
+	}
+
+
+	let events;
+	if(activeFilter == filter.club) {
+		events = await prisma.event.findMany({
+			where: {
+				club: {
+					clubUsers: {
+						some: {
+							userId: user.id
+						}
 					}
 				}
 			}
-		}
-	})
+		})
+	} else if (activeFilter == filter.org) {
+		events = await prisma.event.findMany({
+			where: {
+				club: {
+					organization: {
+						orgUsers: {
+							some: {
+								userId: user.id
+							}
+						}
+					}
+				}
+			}
+		})
+	}
+	
 
 	return {
 		events
