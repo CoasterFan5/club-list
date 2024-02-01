dcc8f8c108b7af937a0c1440dfef3e01d33192ca
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-01 07:24:23 -0500

    calendar that shows all events for all clubs you are in

--- src/routes/(app)/calendar/+page.server.ts
+++ src/routes/(app)/calendar/+page.server.ts
@@ -1,3 +1,23 @@
-export const load = () => {
-	return {}
+import { prisma } from "$lib/server/prismaConnection"
+import { verifySession } from "$lib/server/verifySession"
+
+export const load = async ({cookies}) => {
+
+	const user = await verifySession(cookies.get("session"))
+
+	const events = await prisma.event.findMany({
+		where: {
+			club: {
+				clubUsers: {
+					some: {
+						userId: user.id
+					}
+				}
+			}
+		}
+	})
+
+	return {
+		events
+	}
 }
---
+++ src/routes/(app)/calendar/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Calendar from "$lib/components/Calendar.svelte";
+
+	export let data;
+</script>
+
+<Calendar bind:events={data.events}/>
