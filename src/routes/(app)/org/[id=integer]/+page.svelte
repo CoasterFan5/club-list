a77dc532d5f1e27bcc160a65528fd16661e829b4
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-02 16:00:22 -0500

    format

--- src/routes/(app)/org/[id=integer]/+page.svelte
+++ src/routes/(app)/org/[id=integer]/+page.svelte
@@ -49,49 +49,43 @@
 	};
 
 	const addShareModel = () => {
-
 		addToast({
-			type: "success",
-			message: "Sharing is caring!",
-			life: 5000,
-			
-		})
+			type: 'success',
+			message: 'Sharing is caring!',
+			life: 5000
+		});
 
-
-		window.removeEventListener("mousedown", addShareModel)
-		window.removeEventListener("focus", addShareModel)
-	}
+		window.removeEventListener('mousedown', addShareModel);
+		window.removeEventListener('focus', addShareModel);
+	};
 
 	const startShare = async () => {
 		let shareUrlId = data.org.slug || data.org.id;
-		let shareUrl = `${window.origin}/org/${shareUrlId}`
+		let shareUrl = `${window.origin}/org/${shareUrlId}`;
 
 		try {
 			await navigator.share({
 				url: shareUrl
-			})
-			window.addEventListener("mousedown", addShareModel)
-			window.addEventListener("focus", addShareModel)
-		} catch(e) {
+			});
+			window.addEventListener('mousedown', addShareModel);
+			window.addEventListener('focus', addShareModel);
+		} catch (e) {
 			try {
-				navigator.clipboard.writeText(shareUrl) 
-					addToast({
-					type: "success",
-					message: "URL copied to clipboard",
-					life: 3000,
-				})
+				navigator.clipboard.writeText(shareUrl);
+				addToast({
+					type: 'success',
+					message: 'URL copied to clipboard',
+					life: 3000
+				});
 			} catch (e2) {
 				addToast({
-					type: "error",
-					message: "Your web browser settings disallow sharing.",
-					life: 3000,
-				})
+					type: 'error',
+					message: 'Your web browser settings disallow sharing.',
+					life: 3000
+				});
 			}
 		}
-		
-		
-		
-	}
+	};
 
 	$: handleForm(form, 'Success!', {
 		callback: (form) => {
