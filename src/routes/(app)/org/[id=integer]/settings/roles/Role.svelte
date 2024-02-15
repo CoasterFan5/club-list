0843caac23827087e2e068ce43ab3ebb70ca7f2a
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-02-15 16:56:05 -0500

    ui fixes

--- src/routes/(app)/org/[id=integer]/settings/roles/Role.svelte
+++ src/routes/(app)/org/[id=integer]/settings/roles/Role.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { tooltip } from '$lib/components/tooltips/tooltip';
 
 	export let role: {
 		id: number;
@@ -25,6 +26,7 @@
 	class="role"
 	action="?/updateRole"
 	method="post"
+	style="--dotColor: {dotColor}"
 	use:enhance={() => {
 		// Keep all form data
 		return async ({ update }) => {
@@ -32,30 +34,56 @@
 		};
 	}}
 >
-	<input name="roleId" hidden value={role.id} />
-	<button style="--dotColor: {dotColor}" class="dot" on:click={openColorInput}>
+	<div class="left" >
+		<input name="roleId" hidden value={role.id} />
+		<button  class="dot" on:click={openColorInput}>
+			<input
+				bind:this={colorInput}
+				name="color"
+				class="colorInput"
+				type="color"
+				on:change={valueChanged}
+				bind:value={dotColor}
+			/>
+		</button>
 		<input
-			bind:this={colorInput}
-			name="color"
-			class="colorInput"
-			type="color"
+			name="name"
+			class="nameBox"
+			autocomplete="off"
+			placeholder="New Role"
+			value={role.name}
 			on:change={valueChanged}
-			bind:value={dotColor}
 		/>
-	</button>
-	<input
-		name="name"
-		class="nameBox"
-		autocomplete="off"
-		placeholder="New Role"
-		value={role.name}
-		on:change={valueChanged}
-	/>
+	</div>
+	<div class="right">
+		<button type="button" class="iconButton">
+			<img src="/icons/palette.svg" alt="palette" title="Color" use:tooltip={"Color"}>
+		</button>
+		<button type="button" class="iconButton">
+			<img src="/icons/key.svg" alt="key" title="Permissions" use:tooltip={"Permissions"}>
+		</button>
+	</div>
+	
 
 	<button bind:this={submitButton} hidden type="submit" />
 </form>
 
 <style lang="scss">
+	.left {
+		width: 100%;
+		flex-wrap: nowrap;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: start;
+	}
+	.right {
+		flex-wrap: nowrap;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: end;
+	}
 	.role {
 		display: flex;
 		width: 100%;
@@ -112,4 +140,38 @@
 			border: 1px solid var(--accent);
 		}
 	}
+
+	.iconButton {
+		all: unset;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 50%;
+		aspect-ratio: 1/1;
+		padding: 5px;
+		height: 100%;
+		box-sizing: border-box;
+		position: relative;
+		z-index: 1;
+		cursor: pointer;
+
+		&::after {
+			content: "";
+			position: absolute;
+			height: 100%;
+			left: 100%;
+			top: 0px;
+			left: 0px;
+			aspect-ratio: 1/1;
+			opacity: 0.5;
+			border-radius: 5px;
+			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
+		}
+
+		&:hover::after {
+			background: var(--dotColor);
+			z-index: -1;
+		}
+	}
+	
 </style>
