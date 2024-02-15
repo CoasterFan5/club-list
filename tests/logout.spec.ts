6cce9853ef609ea906d8fa3f718f8731a035dc74
LeoDog896 <LeoDog896@hotmail.com>
2024-02-15 14:53:58 +0000

    test: reuse page between

--- tests/logout.spec.ts
+++ tests/logout.spec.ts
@@ -1,15 +1,25 @@
-import { test } from '../playwright/fixtures';
+import { type Page, test } from '../playwright/fixtures';
 
 test.describe.configure({ mode: 'serial' });
 
-test('logout works', async ({ page }) => {
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test('logout works', async () => {
 	await page.goto('/profile');
 	await page.locator('text=Log Out').click();
 
 	await page.waitForURL('/login');
 });
 
-test("can't navigate back to profile", async ({ page }) => {
+test("can't navigate back to profile", async () => {
 	await page.goto('/profile');
 	await page.waitForURL('/login');
 });
