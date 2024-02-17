1f5bcfc3c15b28c3715a139c096c113621920968
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-02-16 20:19:39 -0500

    test: base org and dashboard spec

---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from './fixtures';
+
+test('dashboard page has base greeting & no organizations', async ({ page }) => {
+	await page.goto('/dashboard');
+
+	await expect(page.getByText('Welcome back, Test!', { exact: true })).toBeVisible();
+    await expect(page.getByText('You are not in any organizations.', { exact: true })).toBeVisible();
+});
---
+++ tests/org.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from './fixtures';
+
+test.describe.configure({ mode: 'parallel' });
+
+test('organizations page has nothing', async ({ page }) => {
+	await page.goto('/org');
+
+	await expect(page.getByText('No Joined Organizations', { exact: true })).toBeVisible();
+    await expect(page.getByText('Join or Create one!', { exact: true })).toBeVisible();
+});
+
+test('organizations can be made', async ({ page }) => {
+    await page.goto('/org');
+
+    await page.getByText('Create', { exact: true }).click();
+    await page.waitForSelector('text=Create Organization', { state: 'visible' });
+
+    await page.locator('input[name="name"]').fill('Test Organization');
+    await page.locator('text=Create').click();
+
+    await expect(page.getByText('Test Organization', { exact: true })).toBeVisible();
+});
