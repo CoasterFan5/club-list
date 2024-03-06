8ac4fd6e22c17494d3024990bf6bcab0b5f2f306
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-06 14:00:10 -0500

    fix invite screen format

--- src/routes/invite/[code]/+page.svelte
+++ src/routes/invite/[code]/+page.svelte
@@ -6,28 +6,48 @@
 </script>
 
 <main>
-	<h1>You've been invited to join <span class="accent">{data.org.name}</span></h1>
-	<div class="options">
-		<div class="button">
-			{#if data.isLoggedIn}
-				<form action="/org?/join" method="POST" use:enhance>
-					<input name="joinCode" hidden value={data.joinCode} />
-					<Button value="Join" />
-				</form>
-			{:else}
-				<Button href="/login?invite={data.joinCode}" value="Login to Join" />
-			{/if}
-		</div>
-		<span>or</span>
-		<div class="button">
-			<Button href="/dashboard" value="Go Home" />
+	<div class="card">
+		<div class="title">You've been invited to join: <span class="accent">{data.org.name}</span></div>
+		<div class="options">
+			<div class="button">
+				{#if data.isLoggedIn}
+					<form action="/org?/join" method="POST" use:enhance>
+						<input name="joinCode" hidden value={data.joinCode} />
+						<Button value="Join" />
+					</form>
+				{:else}
+					<Button href="/login?invite={data.joinCode}" value="Login to Join" />
+				{/if}
+			</div>
+			<span class="or">or</span>
+			<div class="button">
+				<Button href="/dashboard" value="Go Home" />
+			</div>
 		</div>
 	</div>
+	
 </main>
 
 <style lang="scss">
+
+	.title {
+		display: flex;
+		flex-direction: column;
+		margin: 25px 0px;
+
+		span {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			font-size: 3rem;
+			margin-top: 5px;
+		}
+	}
 	.button {
-		min-width: 10rem;
+		min-width: 350px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.options {
@@ -35,10 +55,20 @@
 		flex-direction: row;
 		gap: 0.5rem;
 		align-items: center;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.accent {
 		color: var(--accent);
+	}
+
+	.or {
+		text-align: center;
+		font-size: 1.5rem;
+		padding: 0px 25px;
+		width: 100%;
 	}
 
 	main {
@@ -49,5 +79,16 @@
 		align-items: center;
 		flex-direction: column;
 		height: 100vh;
+	}
+
+	.card {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		background: var(--bgMid);
+		padding: 25px;
+		border-radius: 5px;
+		box-sizing: border-box;
+		box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
 	}
 </style>
