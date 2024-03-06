68b78fc24f4596445a3d6e23cefb7223d18df82d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-03-06 15:22:34 -0500

    Fix some inconsistent and weird things. (#108)
    * Fix bad spelling
    
    * update pfp sizing in club member list
    
    * update org settings sidebar, fixes #52
    
    * redirect if you are already in an org and go to org invite
    
    * fix invite screen format
    
    * format (yay)

--- tests/org.spec.ts
+++ tests/org.spec.ts
@@ -6,37 +6,37 @@ test('organizations page has nothing', async ({ page }) => {
 	await page.goto('/org');
 
 	await expect(page.getByText('No Joined Organizations', { exact: true })).toBeVisible();
-    await expect(page.getByText('Join or Create one!', { exact: true })).toBeVisible();
+	await expect(page.getByText('Join or Create one!', { exact: true })).toBeVisible();
 });
 
 test('organizations can be made', async ({ page }) => {
-    await page.goto('/org');
+	await page.goto('/org');
 
-    await page.getByText('Create', { exact: true }).click();
-    await page.waitForSelector('text=Create Organization', { state: 'visible' });
+	await page.getByText('Create', { exact: true }).click();
+	await page.waitForSelector('text=Create Organization', { state: 'visible' });
 
-    await page.locator('input[name="name"]').fill('Test Organization');
-    await page.locator('form').locator('button').locator('text=Create').click();
+	await page.locator('input[name="name"]').fill('Test Organization');
+	await page.locator('form').locator('button').locator('text=Create').click();
 
-    await expect(page.getByText('Test Organization', { exact: true })).toBeVisible();
-    await page.locator('text=Test Organization').click();
-    await page.waitForURL(/\/org\/[0-9]+/);
-    await expect(page.getByText('Test Organization', { exact: true })).toBeVisible();
-    await expect(page.getByText('No clubs here yet.')).toBeVisible();
+	await expect(page.getByText('Test Organization', { exact: true })).toBeVisible();
+	await page.locator('text=Test Organization').click();
+	await page.waitForURL(/\/org\/[0-9]+/);
+	await expect(page.getByText('Test Organization', { exact: true })).toBeVisible();
+	await expect(page.getByText('No clubs here yet.')).toBeVisible();
 });
 
 test('organizations can be joined', async ({ page }) => {
-    await page.goto('/org');
+	await page.goto('/org');
 
-    await page.getByText('Join', { exact: true }).click();
-    await page.waitForSelector('text=Join an Organization', { state: 'visible' });
+	await page.getByText('Join', { exact: true }).click();
+	await page.waitForSelector('text=Join an Organization', { state: 'visible' });
 
-    await page.locator('input[name="joinCode"]').fill('123456');
-    await page.locator('form').locator('button').locator('text=Join').click();
+	await page.locator('input[name="joinCode"]').fill('123456');
+	await page.locator('form').locator('button').locator('text=Join').click();
 
-    await expect(page.getByText('Cardboard', { exact: true })).toBeVisible();
-    await page.locator('text=Cardboard').click();
-    await page.waitForURL(/\/org\/[0-9]+/);
-    await expect(page.getByText('Cardboard', { exact: true })).toBeVisible();
-    await expect(page.getByText('Cardboard Club', { exact: true })).toBeVisible();
+	await expect(page.getByText('Cardboard', { exact: true })).toBeVisible();
+	await page.locator('text=Cardboard').click();
+	await page.waitForURL(/\/org\/[0-9]+/);
+	await expect(page.getByText('Cardboard', { exact: true })).toBeVisible();
+	await expect(page.getByText('Cardboard Club', { exact: true })).toBeVisible();
 });
