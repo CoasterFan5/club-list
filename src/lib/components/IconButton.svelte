7d91e994792fe675fc06799c016744f2084366db
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-10 13:10:58 -0400

    add iconButton

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	/**
+	 * Href and formData are not compatible, use one or the other.
+	 */
+	export let href: string | undefined = undefined;
+
+	/**
+	 * Href and formData are not compatible, use one or the other.
+	 */
+	export let formData:
+		| {
+				method: HTMLFormElement['method'];
+				action: HTMLFormElement['action'];
+		  }
+		| undefined = undefined;
+</script>
+
+{#if href}
+	<a class="iconButton" {href} on:click>
+		<slot />
+	</a>
+{:else if formData}
+	<form class="formButton" action={formData.action} method={formData.method} use:enhance>
+		<button class="iconButton" on:click>
+			<slot />
+		</button>
+	</form>
+{:else}
+	<button class="iconButton" on:click>
+		<slot />
+	</button>
+{/if}
+
+<style lang="scss">
+	.iconButton {
+		all: unset;
+		cursor: pointer;
+		padding: 5px;
+		height: 100%;
+		box-sizing: border-box;
+		margin: 0px;
+		aspect-ratio: 1/1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 50%;
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
+
+		&:hover {
+			background: var(--accent50);
+		}
+
+		svg {
+			height: 100%;
+		}
+	}
+
+	.formButton {
+		all: unset;
+		cursor: pointer;
+		aspect-ratio: 1/1;
+		height: 100%;
+	}
+
+	:global(.iconButton svg) {
+		height: 100%;
+		width: 100%;
+	}
+</style>
--- src/routes/(app)/org/[id=integer]/settings/bans/+page.svelte
+++ src/routes/(app)/org/[id=integer]/settings/bans/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	export let data;
+	import IconButton from "$lib/components/IconButton.svelte"
+
+	let showingInfo = false;
+
+	const openInfo = (e: MouseEvent, ban: typeof data.bans[0]) => {
+		
+	}
+
 </script>
 
 <div class="bans">
 	{#each data.bans as ban}
 		<div class="ban">
-			<img class="pfp" src={ban.user.pfp || "/defaultPFP.png"} alt="Profile Identifier"/>
-			<p class="name">{ban.user.firstName} {ban.user.lastName}</p>
+			<div class="left">
+				<img class="pfp" src={ban.user.pfp || "/defaultPFP.png"} alt="Profile Identifier"/>
+				<p class="name">{ban.user.firstName} {ban.user.lastName}</p>
+			</div>
 			
+			<div class="info">
+				<IconButton on:click={(e) => {openInfo(e, ban)}}>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path></svg>
+				</IconButton>
+			</div>
 		</div>
 	{/each}
 </div>
@@ -30,7 +45,12 @@
 		display: flex;
 		flex-direction: row;
 
-		.name {
+		.left {
+			display: flex;
+			flex-direction: row;
+			width: 100%;
+
+			.name {
 			display: flex;
 			align-items: center;
 			justify-content: center;
@@ -38,11 +58,19 @@
 			margin: 0px;
 			font-size: 1.2rem;
 		}
+		}
+		
 	}
 	.pfp {
 		aspect-ratio: 1/1;
 		height: 32px;
 		border: 1px solid var(--accent);
 		border-radius: 50%;
+	}
+
+	
+
+	button {
+		display: flex;
 	}
 </style>
