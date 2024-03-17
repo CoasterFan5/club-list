9393643219ee85fd8ee65c59a9a29a0bd6df8eee
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-03-17 17:49:53 -0400

    Make feedback form better (#135)
    * refactor smile scale component
    
    * add more questions to the feedback form
    
    * update server to deal with new feedback form
    
    * fix some small inconsistencies
    
    * format

--- src/routes/(app)/feedback/+page.server.ts
+++ src/routes/(app)/feedback/+page.server.ts
@@ -9,10 +9,12 @@ const colorScale = [0xe38987, 0xe69a67, 0xcfb44f, 0x9ecf56, 0x36e789];
 export const actions = {
 	submitFeedback: formHandler(
 		z.object({
-			rating: z.coerce.number(),
+			generalRating: z.coerce.number(),
+			bugRating: z.coerce.number(),
+			designRating: z.coerce.number(),
 			description: z.string()
 		}),
-		async ({ rating, description }, { cookies }) => {
+		async ({ generalRating, bugRating, designRating, description }, { cookies }) => {
 			const user = await prisma.user.findFirst({
 				where: {
 					sessions: {
@@ -23,33 +25,48 @@ export const actions = {
 				}
 			});
 
+			const avgRating = Math.max(Math.round((generalRating + bugRating + designRating) / 3), 1);
+
 			const name = user ? `${user.firstName} ${user.lastName} - ${user.email}` : 'No user';
 			const pfp = user?.pfp ? user.pfp : `https://clubsaur.us/defaultPFP.png`;
-			const color = colorScale[rating - 1] || 0xffffff;
+			const color = colorScale[avgRating - 1] || 0xffffff;
 
-			await fetch(DISCORDURL, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					content: null,
-					attachments: [],
-					embeds: [
-						{
-							title: 'New report',
-							type: 'rich',
-							description: `**Rating:** ${rating}/5\n**Description:** ${description}`,
-							color: color,
-							timestamp: new Date().toISOString(),
-							author: {
-								name: name,
-								icon_url: pfp
+			try {
+				await fetch(DISCORDURL, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						content: null,
+						attachments: [],
+						embeds: [
+							{
+								title: 'New report',
+								type: 'rich',
+								description: `**General Rating**: ${generalRating}/5\n**Bug Rating**: ${bugRating}/5\n **Design Rating**: ${designRating}\n`,
+								fields: [
+									{
+										name: 'Other Comments',
+										value: description
+									}
+								],
+								color: color,
+								timestamp: new Date().toISOString(),
+								author: {
+									name: name,
+									icon_url: pfp
+								}
 							}
-						}
-					]
-				})
-			});
+						]
+					})
+				});
+			} catch (e) {
+				return {
+					success: false,
+					message: 'Something went wrong...'
+				};
+			}
 
 			return {
 				success: true,
