341f843bf817964bb698434a37d8cc12d800556a
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-17 20:26:09 -0400

    I forgot what this commit does ngl

--- src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.server.ts
+++ src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.server.ts
@@ -1,3 +1,47 @@
-export const load = () => {
-	
+import { redirect } from '@sveltejs/kit';
+
+import { prisma } from '$lib/server/prismaConnection.js';
+
+
+export const load = async ({parent, params}) => {
+	const parentData = await parent();
+	if(!parentData.clubPerms.viewAttendance) {
+		throw redirect(303, `/org/${params.id}/club/${params.clubId}`)
+	}
+
+	const events = await prisma.clubAttendanceEvent.findMany({
+		where: {
+			clubId: parentData.club.id
+		}
+	})
+
+	const members = await prisma.clubUser.findMany({
+		where: {
+			clubId: parentData.club.id
+		},
+		select: {
+			user: {
+				select: {
+					firstName: true,
+					lastName: true,
+					pfp: true,
+					attendanceMarks: {
+						where: {
+							clubId: parentData.club.id
+						},
+						select: {
+							attendanceEvent: true
+						}
+					}
+				}
+			}
+		},
+		
+	})
+
+
+	return {
+		attendanceMembers: members,
+		attendanceEvents: events,
+	}
 }
--- src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.svelte
+++ src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.svelte
@@ -1,9 +1,38 @@
-<div class="wrap">
+<script lang="ts">
+	export let data;
+	
+	
+</script>
 
+<div class="wrap">
+	{#each data.attendanceMembers as attendanceMember}
+		<div class="attendanceItem">
+			<p>{attendanceMember.user.firstName} {attendanceMember.user.lastName}</p>
+			
+			{#each data.attendanceEvents as attendanceEvent }
+				<div class="attendanceData">
+					
+				</div>
+			{/each}
+		</div>	
+	{/each}
 </div>
 
-<style>
+<style lang="scss">
 	.wrap {
-		width: 100%;
+		width: 90%;
+		margin-top: 50px;
+	}
+
+	.attendanceItem {
+		margin: 0.25rem 0rem;
+		background: var(--bgMid);
+		padding: 0.4rem;
+		border-radius: 5px;
+
+		p {
+			margin: 0px;
+			font-size: 1.2rem;
+		}
 	}
 </style>
