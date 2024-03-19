29b023255a784008b556e654fa00890311771860
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-19 19:45:44 -0400

    dashboard image

--- src/routes/(home)/Dashboard.svelte
+++ src/routes/(home)/Dashboard.svelte
@@ -3,5 +3,37 @@
 </script>
 
 <div class="wrap">
-	<enhanced:img src="/dashboard.jpg" alt="Dashboard"/>
+	<div class="image">
+		<img src={dashboardImage} alt="Dashboard" />
+	</div>
+	
 </div>
+
+<style lang="scss">
+	.wrap {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-top: -20vh;
+
+		.image {
+			width: 80%;
+			min-width: 300px;
+			border: 3px solid var(--accent);
+			border-radius: 1rem;
+			overflow: hidden;
+			box-shadow: 1px 1px 3px 3px rgba(0, 0, 0, 0.1);
+
+			img {
+				width: 100%;
+				object-fit: fit;
+				display: block;
+				height: auto;
+				
+			}
+		}
+	}
+
+
+</style>
