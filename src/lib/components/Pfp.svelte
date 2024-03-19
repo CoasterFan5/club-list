81bc3b129a3328f3cb4859e1dd3ebdf64f591c37
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-19 05:24:02 -0400

    add pfp component and fix some styling

---
+++ src/lib/components/Pfp.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let pfp: string | null;
+	export let height = "24px";
+</script>
+
+<img class="pfp" alt="profile" src={pfp || '/defaultPFP.png'} style="height: {height};" />
+
+<style class="scss">
+	.pfp {
+		border-radius: 50%;
+		border: 1px solid var(--accent);
+		margin-right: 10px;
+		aspect-ratio: 1 / 1;
+		object-fit: cover;
+	}
+</style>
--- src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.svelte
+++ src/routes/(app)/org/[id=integer]/club/[clubId]/attendance/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
+	import Pfp from "$lib/components/Pfp.svelte";
+	import Button from "$lib/components/Button.svelte"
 	export let data;
 	
 	
 </script>
 
 <div class="wrap">
+	<Button value="Add Attendance Event"/>
 	{#each data.attendanceMembers as attendanceMember}
 		<div class="attendanceItem">
-			<p>{attendanceMember.user.firstName} {attendanceMember.user.lastName}</p>
+			<div class="user">
+				<Pfp pfp={attendanceMember.user.pfp}/>
+				<p>{attendanceMember.user.firstName} {attendanceMember.user.lastName}</p>
+			</div>
+			
 			
 			{#each data.attendanceEvents as attendanceEvent }
 				<div class="attendanceData">
-					
+					<h1></h1>
 				</div>
 			{/each}
+
+			<form>
+
+			</form>
+			
 		</div>	
 	{/each}
 </div>
@@ -25,14 +37,21 @@
 	}
 
 	.attendanceItem {
-		margin: 0.25rem 0rem;
+		margin: 0.3rem 0rem;
 		background: var(--bgMid);
 		padding: 0.4rem;
 		border-radius: 5px;
+		box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.05);
 
 		p {
 			margin: 0px;
 			font-size: 1.2rem;
 		}
+	}
+
+	.user {
+		display: flex;
+		align-items: center;
+		justify-content: start;
 	}
 </style>
