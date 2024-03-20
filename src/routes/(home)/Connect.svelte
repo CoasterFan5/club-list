8ed08b2a2415b887d19fe4b6bd2122e0f18eff5b
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-03-20 05:24:36 -0400

    added section about security, privacy, and moderation

--- src/routes/(home)/Connect.svelte
+++ src/routes/(home)/Connect.svelte
@@ -8,17 +8,23 @@
 		{
 			title: "Security",
 			icon: LockIcon,
-			color: "#0335fc"
+			color: "#0335fc",
+			open: false,
+			desc: "At Clubsaurus, we understand the importance of security regarding our users' data. That's why we have prioritized ensuring that we build our platform with the highest level of protection possible. We are proud to say that our entire codebase is open to the public for review and analysis. This openness provides transparency into our security practices and allows us to receive unparalleled feedback from our community of developers who can help us identify and address potential vulnerabilities. Being open about our security practices can create a safer and more trustworthy environment for our users.",
 		},
 		{
 			title: "Moderation",
 			icon: ShieldIcon,
-			color: "#008a32"
+			color: "#008a32",
+			open: false,
+			desc: "Moderating user-generated content can be challenging, but with Clubsaurus, it becomes much more manageable. Only authorized users are allowed to post content within a Clubsaurus organization by default. This measure is put in place to protect you and your students from being exposed to harmful content posted by malicious individuals. Moreover, we grant organization administrators the ability to ban any user they deem necessary. This control allows you complete authority over the content posted in your organization and ensures that your community remains safe and secure. ",
 		},
 		{
 			title: "Privacy",
 			icon: HideIcon,
-			color: "#c77100"
+			color: "#c77100",
+			open: false,
+			desc: "At Clubsaurus, we recognize that our users' privacy is essential and are committed to ensuring it is always protected. That is why we have developed our platform to offer users a wide range of privacy options. With Clubsaurus, you can decide what data you want to share about yourself and your students. You are in complete control of your information, and we are committed to keeping it safe and secure. Our goal is to provide a safe and enjoyable experience for everyone who uses Clubsaurus, and we believe that privacy is a critical factor in achieving that goal.",
 		}
 	]
 
@@ -28,14 +34,22 @@
 	<div class="inner">
 		<h2>Protect student experience in clubs.</h2>
 		{#each sections as section}
-			<button class="section" style="--color: {section.color};" >
-				<div class="icon">
-					<svelte:component this={section.icon} color={section.color} height="100%" width="100%"/>
+			<button class="section" class:active={section.open} style="--color: {section.color};" on:click={() => {section.open = !section.open}} >
+				<div class="sectionMain">
+					<div class="icon">
+						<svelte:component this={section.icon} color={section.color} height="100%" width="100%"/>
+					</div>
+					<h3>{section.title}</h3>
+					<div class="chevron">
+						<Chevron height="100%" width="100%"/>
+					</div>
 				</div>
-				<h3>{section.title}</h3>
-				<div class="chevron">
-					<Chevron height="100%" width="100%"/>
+				
+				{#if section.open}
+				<div>
+					<p>{section.desc}</p>
 				</div>
+				{/if}
 				
 			</button>
 		{/each}
@@ -61,8 +75,7 @@
 	}
 	.section {
 		all: unset;
-		display: flex;
-		align-items: center;
+		
 		padding-left: 1rem;
 		margin: 0.3rem 0px;
 		border-radius: 5px;
@@ -71,13 +84,18 @@
 		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
 		cursor: pointer;
 
+		.sectionMain {
+			display: flex;
+		align-items: center;
+		}
 		
 		&:hover {
 			background: var(--bgPure);
 			box-shadow: 1px 1px 3px 3px rgba(0, 0, 0, 0.1);
 		}
 		
-		.icon {
+	}
+	.icon {
 			margin-right: 0.5rem;
 			height: 2rem;
 			width: 2rem;
@@ -120,6 +138,12 @@
 			position: relative;
 			z-index: 1;
 			margin-left: 0.25rem;
+			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
+		}
+
+	.active {
+		.chevron {
+			transform: rotate(180deg);
 		}
 	}
 </style>
