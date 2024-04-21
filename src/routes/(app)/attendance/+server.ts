e60d9f4af6a31a046f0468e858c021cecd1c5a45
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-04-21 19:36:48 -0400

    Attendance (#140)
    * Don't allow database to be re-seeded if data already exists
    
    * Prisma schema and layout
    
    * I forgot what this commit does ngl
    
    * Dont throw error id db is already seeded
    
    * Update seed to be better
    
    * add pfp component and fix some styling
    
    * Componentize checkbox and basics for server
    
    * changing attendance on server
    
    * checking and unchecking in ui
    
    * style fixes
    
    * fix weird position thing
    
    * refactor to only show 1 attendance event at a time
    
    * add combo box
    
    * add data to combo box
    
    * added ability to switch viewed events
    
    * make attendance display better
    
    * format
    
    * I didnt let format finish
    
    * fix problematic key usage
    
    * rename attendance events
    
    * allow deletion of attendance events
    
    * fix tooltip content
    
    * add creation of qr code code
    
    * add qr code w/ new library
    
    * marking present through qr codes
    
    * format
    
    * fixed up qr display and added beta badge
    
    * Update package.json
    
    * style: capitalize generics
    
    ill get you
    
    * fix: use svelte reactive instead of subscribe
    
    allows for compile-time reduction
    
    * associate attendance events with events
    
    * format
    
    * fix date integration for attendance events
    
    * allow deleting qr codes
    
    * improve mobile compatibility
    
    * add beta banner and fix tooltips
    
    * Start on mini calendar
    
    * fix up mini calendar
    
    * format & cleanup
    
    * Add a comment explaining grid size math
    
    * remove un-needed -webkit stuff
    
    * remove reduntant !important
    
    * fix attendance qr session verification logic
    
    * drop useless ternary
    
    * remove 0rem uses
    
    * Make database already seeded message error and end early.
    
    * Named tuple in combo box
    
    * use iconify instead of an svg in como box
    
    * Resolve timezone issue in creating new attendance events
    
    ---------
    
    Co-authored-by: Tristan F <26509014+LeoDog896@users.noreply.github.com>

---
+++ src/routes/(app)/attendance/+server.ts
@@ -0,0 +1,56 @@
+import { error, redirect } from '@sveltejs/kit';
+
+import { getClubUserFromSession } from '$lib/server/getClubUserFromSession.js';
+import { prisma } from '$lib/server/prismaConnection.js';
+
+export const GET = async ({ url, cookies }) => {
+	const eventId = url.searchParams.get('id');
+	if (!eventId) {
+		throw error(404, 'No event');
+	}
+
+	const code = url.searchParams.get('code');
+	if (!code) {
+		throw error(404, 'No code');
+	}
+
+	const event = await prisma.clubAttendanceEvent.findFirst({
+		where: {
+			AND: {
+				id: parseInt(eventId),
+				attendanceCode: code
+			}
+		}
+	});
+
+	if (!event) {
+		throw error(404, 'Invalid ID');
+	}
+
+	const clubUser = await getClubUserFromSession(cookies.get('session'), event.clubId.toString());
+
+	if (!clubUser) {
+		throw error(404, 'Not in club');
+	}
+
+	await prisma.clubAttendanceCheck.upsert({
+		where: {
+			clubId_userId_attendanceEventId: {
+				userId: clubUser.clubUser.userId,
+				clubId: event.clubId,
+				attendanceEventId: event.id
+			}
+		},
+		create: {
+			userId: clubUser.clubUser.userId,
+			clubId: event.clubId,
+			attendanceEventId: event.id
+		},
+		update: {}
+	});
+
+	throw redirect(
+		303,
+		`/attendance/good?id=${event.id}&club=${clubUser.clubUser.clubId}&org=${clubUser.clubUser.organizationId}`
+	);
+};
