ec59892d5df0d3065a991c15b121cfb2df9e5161
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-05-24 21:52:35 -0400

    adds a simple hover effect to articles

---
+++ src/lib/components/LavaLampBG.svelte
@@ -0,0 +1,149 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	export let orbCount = 25;
+	export let baseRadius = 50;
+	export let addedRadius = 50;
+	export let baseVel = 0.5;
+	export let addedVel = 0.5;
+
+	export let dataUrl: string = ""
+
+	export let alphaThreshold = 200;
+
+	let canvasWidth: number;
+	let canvasHeight: number;
+
+	$: console.log(canvasHeight)
+
+	
+
+	let canvasElement: HTMLCanvasElement;
+	let fakeCanvas: HTMLCanvasElement;
+
+
+
+	let context: CanvasRenderingContext2D | null;
+	let finalContext: CanvasRenderingContext2D | null;
+	$: if(canvasElement) {
+		context = fakeCanvas.getContext("2d")
+		finalContext = canvasElement.getContext("2d")
+	}
+
+	class Orb {
+		x: number;
+		y: number;
+		xVelocity: number;
+		yVelocity: number;
+		radius: number;
+		gradient: CanvasGradient;
+		constructor() {
+			this.x = (Math.random() * canvasWidth)
+			this.y = (Math.random() * 150)
+			this.radius = baseRadius + addedRadius * Math.random()
+
+			let color = 'hsla(355, 78%, light%, alp)'.replace( 'light', (56 + (Math.floor(Math.random() * 40) - 20)).toString());
+			this.gradient = context?.createRadialGradient(0, 0, 0, 0, 0, this.radius) as CanvasGradient;
+			this.gradient.addColorStop(0, color.replace('alp', `1`))
+			this.gradient.addColorStop(1, color.replace('alp', `0`))
+
+			let radiant = Math.random() * Math.PI * 2;
+			let velocity = baseVel + (addedVel * Math.random())
+
+			this.xVelocity = Math.cos(radiant) * velocity;
+			this.yVelocity = Math.sin(radiant) * velocity;
+		}
+
+		step() {
+			if(!context) {
+				
+				return
+			}
+
+			this.x += this.xVelocity
+			this.y += this.yVelocity
+
+			let radius = this.radius / 2;
+
+			if(this.x < -radius || this.x > canvasWidth + radius) {
+				this.xVelocity = this.xVelocity * -1
+			}
+
+			if(this.y < -radius || this.y > canvasHeight + radius) {
+				this.yVelocity = this.yVelocity * -1
+			}
+
+			
+			context.fillStyle = this.gradient;
+			context.translate(this.x, this.y)
+			context.beginPath();
+			context.arc(0, 0, this.radius, 0, Math.PI * 2);
+			context.fill();
+			context.translate(-this.x, -this.y)
+		}
+	}
+
+	const orbs: Orb[] = [];
+	
+
+
+	let animation = () => {
+		
+		
+		window.requestAnimationFrame(animation);
+
+		if(!context || !finalContext) {
+			return;
+		}
+
+		
+
+
+		if(orbs.length == 0) {
+			for(let i = 0; i < orbCount; i++) {
+				orbs.push(new Orb())
+			}
+			console.log("orbs created")
+		}
+
+
+		
+		context.clearRect(0, 0, canvasWidth, canvasHeight)
+		finalContext.clearRect(0, 0, canvasWidth, canvasHeight)
+
+		for(let i = 0; i < orbs.length; i++) {
+			orbs[i].step()
+		}
+
+		let image = context.getImageData(0, 0, canvasWidth, canvasHeight);
+		let imageData = new Uint8Array(image.data.buffer);
+
+		for(let i = 3; i < imageData.length; i += 4) {
+			imageData[i] /= imageData[i] < alphaThreshold ? 6 : 1;
+		}
+
+		finalContext.putImageData(image, 0, 0)
+	}
+	
+	onMount(() => {
+		animation()
+	})
+
+	
+
+	
+</script>
+
+<canvas hidden width={canvasWidth} height={canvasHeight} bind:this={fakeCanvas}></canvas>
+<canvas bind:clientHeight={canvasHeight} bind:clientWidth={canvasWidth} width={canvasWidth} height={canvasHeight} bind:this={canvasElement} id="final">
+	
+</canvas>
+
+<style lang="scss">
+	canvas {
+		background: transparent;
+		margin-bottom: 1rem;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/blog/(articles)/+page.svelte
+++ src/routes/blog/(articles)/+page.svelte
@@ -1,16 +1,48 @@
 <script lang="ts">
+	import LavaLampBg from "$lib/components/LavaLampBG.svelte";
+	let showingLavaLamp = false;
+
 	export let data;
+
+	let lavaLampHolder: HTMLDivElement;
+
+	const configureLavaLampPosition = (e: MouseEvent) => {
+		showingLavaLamp = true;
+		(e.target as HTMLElement ).appendChild(lavaLampHolder)
+	}
+
+	const hideLavaLamp = (e: MouseEvent) => {
+		showingLavaLamp = false;
+	}
 </script>
+
+<div bind:this={lavaLampHolder} hidden={!showingLavaLamp} class="lavaLamp">
+	<LavaLampBg/>		
+</div>
+
+
+
 <div class="wrap">
 	<h2>Clubsaurus Blog</h2>
 	<div class="articles">
+		
 		{#each data.articles as article}
-			<a href="/blog/{article.articleURL}" class="article">
+		<a href="/blog/{article.articleURL}" class="articleWrap" on:mouseenter={configureLavaLampPosition} on:mouseleave={hideLavaLamp}>
+			<div class="text">
 				<h2>{article.articleName}</h2>
 				<p>This will be the article description, however, I have not yet added short article descriptions.</p>
-			</a>
+				<div class="text2">
+					<h2>{article.articleName}</h2>
+					<p>This will be the article description, however, I have not yet added short article descriptions.</p>
+				</div>
+			</div>
+			
+		</a>
+			
 		{/each}
 	</div>
+
+	
 </div>
 
 
@@ -32,31 +64,88 @@
 		max-width: 70rem;
 	}
 
-	.article {
+	.articleWrap {
+		position: relative;
 		text-decoration: none;
 		padding: 1rem;
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
 
-		h2 {
-			font-size: 1.75rem;
-			margin: 0;
-			color: var(--dark);
-		}
-		p {
-			margin: 0px;
-			margin-top: 1rem;
-			font-size: 1.2rem;
-			line-height: 1.25rem;
-			color: var(--mid);
-		}
+		.text {
+			text-decoration: none;
+			padding: 1rem;
+			box-sizing: border-box;
+			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
+			
+			
+			
 
-		&:hover {
-			h2 {
-				color: var(--accent);
+				& > h2 {
+					font-size: 1.75rem;
+					margin: 0;
+					color: var(--dark);
+					opacity: 0;
+					
+				}
+				& > p {
+					margin: 0px;
+					margin-top: 1rem;
+					font-size: 1.2rem;
+					line-height: 1.25rem;
+					color: var(--mid);
+				}
+
+			
+			}
+		&:hover .text {
+				mix-blend-mode: color-burn;
+				
+				& > h2 {
+					opacity: 0.3;
+				}
+
+				& > p {
+					opacity: 0.3;
+				}
 			}
 
-			p {
-				color: var(--accent);
+		.text2 {
+			color: rgb(255, 0, 0);
+			text-decoration: none;
+			padding: 2rem;
+			box-sizing: border-box;
+			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
+			z-index: -2;
+			position: absolute;
+			top: 0px;
+			left: 0px;
+
+			& > h2 {
+					font-size: 1.75rem;
+					margin: 0;
+					color: var(--dark);
+					
+				}
+			& > p {
+				margin: 0px;
+				margin-top: 1rem;
+				font-size: 1.2rem;
+				line-height: 1.25rem;
+				color: var(--mid);
 			}
 		}
+	}
+
+	
+
+	.lavaLamp {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		border-radius: 0.25rem;
+		overflow: hidden;
+		
 	}
 </style>
