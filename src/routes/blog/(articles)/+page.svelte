7c7774fc6298103b6f1846badeb22ed9f1454c84
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-05-31 14:58:27 -0400

    remove the very un-necessary hover effect

--- src/routes/blog/(articles)/+page.svelte
+++ src/routes/blog/(articles)/+page.svelte
@@ -5,44 +5,19 @@
 	export let data;
 
 	let lavaLampHolder: HTMLDivElement;
-
-	const configureLavaLampPosition = (e: MouseEvent) => {
-		showingLavaLamp = true;
-		(e.target as HTMLElement).appendChild(lavaLampHolder);
-	};
-
-	const hideLavaLamp = (e: MouseEvent) => {
-		showingLavaLamp = false;
-	};
 </script>
-
-<div bind:this={lavaLampHolder} class="lavaLamp" hidden={!showingLavaLamp}>
-	<LavaLampBg />
-</div>
 
 <div class="wrap">
 	<h2>Clubsaurus Blog</h2>
 	<div class="articles">
 		{#each data.articles as article}
-			<a
-				class="articleWrap"
-				href="/blog/{article.articleURL}"
-				on:mouseenter={configureLavaLampPosition}
-				on:mouseleave={hideLavaLamp}
-			>
+			<a class="articleWrap" href="/blog/{article.articleURL}">
 				<div class="text">
 					<h2>{article.articleName}</h2>
 					<p>
 						This will be the article description, however, I have not yet added short article
 						descriptions.
 					</p>
-					<div class="text2">
-						<h2>{article.articleName}</h2>
-						<p>
-							This will be the article description, however, I have not yet added short article
-							descriptions.
-						</p>
-					</div>
 				</div>
 			</a>
 		{/each}
@@ -77,13 +52,12 @@
 			text-decoration: none;
 			padding: 1rem;
 			box-sizing: border-box;
-			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
 
 			& > h2 {
 				font-size: 1.75rem;
 				margin: 0;
 				color: var(--dark);
-				opacity: 0;
+				transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
 			}
 			& > p {
 				margin: 0px;
@@ -94,39 +68,8 @@
 			}
 		}
 		&:hover .text {
-			mix-blend-mode: color-burn;
-
 			& > h2 {
-				opacity: 0.3;
-			}
-
-			& > p {
-				opacity: 0.3;
-			}
-		}
-
-		.text2 {
-			color: rgb(255, 0, 0);
-			text-decoration: none;
-			padding: 2rem;
-			box-sizing: border-box;
-			transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.25s;
-			z-index: -2;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-
-			& > h2 {
-				font-size: 1.75rem;
-				margin: 0;
-				color: var(--dark);
-			}
-			& > p {
-				margin: 0px;
-				margin-top: 1rem;
-				font-size: 1.2rem;
-				line-height: 1.25rem;
-				color: var(--mid);
+				color: var(--accent);
 			}
 		}
 	}
